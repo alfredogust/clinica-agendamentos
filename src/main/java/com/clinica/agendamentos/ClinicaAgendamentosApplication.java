ce2e02dc38dbeaa1b7021fee82e82dc754404a7f
alfredogust <alfredog.fu@gmail.com>
2026-06-09 14:21:03 -0300

    chore: bootstrap project with postgres connection

---
+++ src/main/java/com/clinica/agendamentos/ClinicaAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaAgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaAgendamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/agendamentos/ClinicaAgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaAgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

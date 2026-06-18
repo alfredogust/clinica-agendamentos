ab05e4c7d3fbff63040178f726eb9294f684dd98
alfredogust <alfredog.fu@gmail.com>
2026-06-18 15:20:06 -0300

    feat(professional): add registration endpoint

---
+++ src/main/java/com/clinica/agendamentos/professional/ProfessionalController.java
@@ -0,0 +1,39 @@
+package com.clinica.agendamentos.professional;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.clinica.agendamentos.professional.dto.CreateProfessionalRequest;
+import com.clinica.agendamentos.professional.dto.ProfessionalResponse;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/professionals")
+public class ProfessionalController {
+    
+    private final ProfessionalService professionalService;
+
+    public ProfessionalController(ProfessionalService professionalService) {
+        this.professionalService = professionalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfessionalResponse> register(@Valid @RequestBody CreateProfessionalRequest request) {
+        ProfessionalResponse response = professionalService.register(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+}
